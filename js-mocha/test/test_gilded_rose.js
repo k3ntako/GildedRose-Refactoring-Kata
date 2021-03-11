c45057b4d3983895542f14bd8dec485f09562728
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-11 10:29:16 -0500

    Add new describe block

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -1,9 +1,11 @@
 var { expect } = require("chai");
 var { Shop, Item } = require("../src/gilded_rose.js");
 describe("Gilded Rose", function () {
-  it("should return foo", function () {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
+  describe("updateQuality", function () {
+    it("should return foo", function () {
+      const gildedRose = new Shop([new Item("foo", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).to.equal("foo");
+    });
   });
 });
