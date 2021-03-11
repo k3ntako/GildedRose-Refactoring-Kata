bbc1cf8fe8b8e6bb1a6796458fb93bbb785089e2
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-11 15:46:38 -0500

    Refactor checks for quality over 50

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -24,21 +24,13 @@ class Shop {
           this.items[i].quality = this.items[i].quality - 1;
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
+        this.items[i].quality = this.items[i].quality + 1;
+        if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+          if (this.items[i].sellIn < 11) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+          if (this.items[i].sellIn < 6) {
+            this.items[i].quality = this.items[i].quality + 1;
           }
         }
       }
@@ -58,10 +50,12 @@ class Shop {
               this.items[i].quality - this.items[i].quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+          this.items[i].quality = this.items[i].quality + 1;
         }
+      }
+
+      if (this.items[i].quality > 50) {
+        this.items[i].quality = 50;
       }
     }
 
