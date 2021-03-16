6a4d99b6c4164ab5201bb5b2d617bb2bebb251bd
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-16 10:39:54 -0400

    Simplify check for quality under 0

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,8 +11,7 @@ class GildedRose(object):
                 continue
 
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    item.quality = item.quality - 1
+                item.quality = item.quality - 1
             else:
                 item.quality = item.quality + 1
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
@@ -26,12 +25,14 @@ class GildedRose(object):
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            item.quality = item.quality - 1
+                        item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     item.quality = item.quality + 1
+
+            if item.quality < 0:
+                item.quality = 0
 
             if item.quality > 50:
                 item.quality = 50
