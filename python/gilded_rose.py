9b45bab72cef333a1c2d6420f78fbd64f652f0af
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-16 13:26:39 -0400

    Add instance method for updating generic items

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,26 +9,12 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
-
-            if item.name == "Aged Brie":
+            elif item.name == "Aged Brie":
                 self.update_age_brie(item)
-                continue
-
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 self.update_backstage_passes(item)
-                continue
-
-            item.quality = item.quality - 1
-            item.sell_in = item.sell_in - 1
-
-            if item.sell_in < 0:
-                item.quality = item.quality - 1
-
-            if item.quality < 0:
-                item.quality = 0
-
-            if item.quality > 50:
-                item.quality = 50
+            else:
+                self.update_generic(item)
 
     def update_age_brie(self, item):
         item.quality = item.quality + 1
@@ -53,12 +39,20 @@ class GildedRose(object):
             item.quality = 50        
         if item.sell_in < 0:
             item.quality = 0
-        
-        
-        
 
+    def update_generic(self, item):
+        item.quality = item.quality - 1
+        item.sell_in = item.sell_in - 1
 
+        if item.sell_in < 0:
+            item.quality = item.quality - 1
 
+        if item.quality < 0:
+            item.quality = 0
+
+        if item.quality > 50:
+            item.quality = 50
+        
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
