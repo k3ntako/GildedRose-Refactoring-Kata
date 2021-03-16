0bac8597bedfbc43b5fe4a32c333c3bd7da775b0
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-16 10:47:13 -0400

    Refactor quality update if statement

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -10,15 +10,16 @@ class GildedRose(object):
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
 
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                item.quality = item.quality - 1
-            else:
+            if item.name == "Aged Brie":
                 item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        item.quality = item.quality + 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                item.quality = item.quality + 1
+                if item.sell_in < 11:
+                    item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    item.quality = item.quality + 1
+            else:
+                item.quality = item.quality - 1
 
             item.sell_in = item.sell_in - 1
 
