51e6419ab21dde9f5cd3f705c1d12bb3d90abdda
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-16 13:50:06 -0400

    Create AgedBrie class

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -57,3 +57,48 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class AgedBrie:
+    def __init__(self, name, sell_in, quality):
+        self.item = Item(name=name, sell_in=sell_in, quality=quality)
+
+    def get_name(self): 
+        return self.item.name 
+       
+    def set_name(self, name): 
+        self.item.name = name 
+  
+    def del_name(self): 
+        del self.item.name
+
+    def get_sell_in(self): 
+        return self.item.sell_in 
+       
+    def set_sell_in(self, sell_in): 
+        self.item.sell_in = sell_in 
+  
+    def del_sell_in(self): 
+        del self.item.sell_in 
+
+    def get_quality(self): 
+        return self.item.quality 
+       
+    def set_quality(self, quality): 
+        self.item.quality = quality 
+  
+    def del_quality(self): 
+        del self.item.quality 
+
+    name = property(get_name, set_name, del_name)
+    sell_in = property(get_sell_in, set_sell_in, del_sell_in)
+    quality = property(get_quality, set_quality, del_quality)
+
+    def update_for_next_day(self):
+        self.item.quality = self.item.quality + 1
+
+        if self.item.sell_in < 0:
+            self.item.quality = self.item.quality + 1
+    
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
