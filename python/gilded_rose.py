8082ec8b71bf3fb792ccd03814f157276b7b6d86
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-16 13:23:24 -0400

    Add instance method for updating backstage passes

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,21 +15,14 @@ class GildedRose(object):
                 continue
 
             if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                item.quality = item.quality + 1
-                if item.sell_in < 11:
-                    item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    item.quality = item.quality + 1
-            else:
-                item.quality = item.quality - 1
+                self.update_backstage_passes(item)
+                continue
 
+            item.quality = item.quality - 1
             item.sell_in = item.sell_in - 1
 
             if item.sell_in < 0:
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    item.quality = 0
-                else:
-                    item.quality = item.quality - 1
+                item.quality = item.quality - 1
 
             if item.quality < 0:
                 item.quality = 0
@@ -47,6 +40,23 @@ class GildedRose(object):
 
         if item.quality > 50:
             item.quality = 50
+
+    def update_backstage_passes(self, item):
+        item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+
+        if item.sell_in < 10:
+            item.quality = item.quality + 1
+        if item.sell_in < 5:
+            item.quality = item.quality + 1
+        if item.quality > 50:
+            item.quality = 50        
+        if item.sell_in < 0:
+            item.quality = 0
+        
+        
+        
+
 
 
 class Item:
