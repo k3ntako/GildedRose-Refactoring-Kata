db93cc80ba3cb7649cf4ca26fdc9072eab7f9da4
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-16 13:29:57 -0400

    Reduce duplication of decreasing sell-in

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,7 +9,10 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
-            elif item.name == "Aged Brie":
+            
+            item.sell_in = item.sell_in - 1
+
+            if item.name == "Aged Brie":
                 self.update_age_brie(item)
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 self.update_backstage_passes(item)
@@ -22,14 +25,11 @@ class GildedRose(object):
     def update_age_brie(self, item):
         item.quality = item.quality + 1
 
-        item.sell_in = item.sell_in - 1
-
         if item.sell_in < 0:
             item.quality = item.quality + 1
 
     def update_backstage_passes(self, item):
         item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
 
         if item.sell_in < 10:
             item.quality = item.quality + 1
@@ -40,7 +40,6 @@ class GildedRose(object):
 
     def update_generic(self, item):
         item.quality = item.quality - 1
-        item.sell_in = item.sell_in - 1
 
         if item.sell_in < 0:
             item.quality = item.quality - 1
