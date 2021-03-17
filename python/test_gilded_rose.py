521f1bca19e717d90b3023cdc71a216ab7c3f74a
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-17 11:04:29 -0400

    Test that backstage passes quality should be zero after event

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -113,6 +113,12 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(18, items[0].quality)
 
+    def test_backstage_passes_quality_should_be_zero_after_event(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 15)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
         
 if __name__ == '__main__':
     unittest.main()
