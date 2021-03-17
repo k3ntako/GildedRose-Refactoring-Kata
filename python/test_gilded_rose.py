21106bc73682f5ce017980f2e14480260a3cc56d
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-17 11:03:20 -0400

    Test that backstage passes quality increases by 3 with 5 days left

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -101,6 +101,12 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(17, items[0].quality)
 
+    def test_backstage_passes_quality_increases_by_three_with_five_days_left(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 15)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(18, items[0].quality)
+
         
 if __name__ == '__main__':
     unittest.main()
